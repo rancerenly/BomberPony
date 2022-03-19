fccc9fa8be4b97845a6646054e0b00ff58557742
Dmitry Savelyev <64603347+FirstAdamsBee@users.noreply.github.com>
2022-03-19 16:20:53 +0700

    Create classes for object instantiation #SU-1

---
+++ Assets/Scripts/Creators/CommandCreator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Creators
+{
+    /// <summary>
+    /// Class that creates objects on command.
+    /// </summary>
+    public class CommandCreator<T> : ObjectCreator<T> where T : Object
+    {
+        [SerializeField]
+        [Tooltip("Name of a command")]
+        private string commandName;
+
+        /// <summary>
+        /// Method that invokes on input event.
+        /// </summary>
+        public virtual void OnCommand(InputAction.CallbackContext callbackContext)
+        {
+            if (callbackContext.performed)
+            {
+                Instantiate();
+            }
+        }
+    }
+}
