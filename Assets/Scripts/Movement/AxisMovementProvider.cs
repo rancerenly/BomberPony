31a1c89ab8c58ae0110a7a8c51a430bc01ce723c
Dmitry Savelyev <64603347+FirstAdamsBee@users.noreply.github.com>
2022-03-19 16:20:16 +0700

    Create movement #SU-1

---
+++ Assets/Scripts/Movement/AxisMovementProvider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Movement
+{
+    /// <summary>
+    /// Class that moves object by axis.
+    /// </summary>
+    public class AxisMovementProvider : MovementProvider
+    {
+        /// <inheritdoc/>
+        public override void Move()
+        {
+            Rigidbody2D.position += Axis * Speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/MovementProvider.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Movement
+{
+    /// <summary>
+    /// Class used for movement handling.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody2D))]
+    public abstract class MovementProvider : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Movement speed.")]
+        protected float Speed;
+    
+        /// <summary>
+        /// Rigidbody2D reference.
+        /// </summary>
+        protected Rigidbody2D Rigidbody2D;
+
+        /// <summary>
+        /// Current input axis.
+        /// </summary>
+        protected Vector2 Axis { get; private set; }
+
+        protected virtual void Awake()
+        {
+            Rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        /// <summary>
+        /// Method that updates axis on input.
+        /// </summary>
+        public void OnMovementChanged(InputAction.CallbackContext callbackContext)
+        {
+            Axis = callbackContext.ReadValue<Vector2>();
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            Move();
+        }
+    
+        /// <summary>
+        /// Method responsible for movement.
+        /// </summary>
+        public abstract void Move();
+    }
+}
