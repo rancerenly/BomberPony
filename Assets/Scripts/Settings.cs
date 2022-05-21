47b891f33cb3746221ff6c0d89c4b3e04991fd64
rancerenly <rancerenly@gmail.com>
2022-05-21 16:46:48 +0700

    Created Main Menu

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public AudioMixer am;
+
+    public void AudioVolume(float sliderValue)
+    {
+        am.SetFloat("masterVolume", sliderValue);
+    }
+}
